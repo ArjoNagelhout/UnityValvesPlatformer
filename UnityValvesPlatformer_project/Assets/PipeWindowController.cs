08d19e713a39dbb6e8ff59ae49f335d61c046e4e
ArjoNagelhout <arjo.nagelhout@gmail.com>
2020-06-27 16:40:23 +0200

    Created windows and pipes

---
+++ UnityValvesPlatformer_project/Assets/PipeWindowController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeWindowController : MonoBehaviour
+{
+    public float minFill;
+    public float maxFill;
+
+    public Renderer liquidRenderer;
+
+    public ValvesController valvesController;
+    public ValveController valveController;
+
+    private float targetPercentage;
+
+    private float currentPercentage;
+    public float changeSpeed;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (valveController.active)
+        {
+            targetPercentage = valvesController.currentPercentage;
+        } else
+        {
+            targetPercentage = 0f;
+        }
+        //Debug.Log(displayPercentage);
+
+        currentPercentage = Mathf.Lerp(currentPercentage, targetPercentage, changeSpeed);
+
+        float actualFill = Remap(currentPercentage, 0f, 1f, minFill, maxFill);
+        liquidRenderer.material.SetFloat("_FillAmount", actualFill);
+        //Debug.Log(liquidRenderer.material.GetFloat("_FillAmount"));
+    }
+
+    private float Remap(float value, float start1, float stop1, float start2, float stop2)
+    {
+        return start2 + (stop2 - start2) * ((value - start1) / (stop1 - start1));
+    }
+}
